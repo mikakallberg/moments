6bda0c0ee2566c62de978738c4efb57172757261
mikakallberg <mikaela.kallberg@hotmail.com>
2022-08-04 19:08:12 +0000

    Add sign up and sign in

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+
+axios.defaults.baseURL = 'https://moments-drf-api-app.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
