29c595e658474ff8230e2634ad1a92549bca23bc
mikakallberg <mikaela.kallberg@hotmail.com>
2022-08-07 16:58:46 +0000

    axios interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -5,3 +5,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://8000-mikakallberg-restdjango-vb9oskvgge5.ws-eu59.gitpod.io'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
