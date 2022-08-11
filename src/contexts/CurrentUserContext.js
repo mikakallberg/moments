740238908711c3f9fb97b3f4c401ea912fadd419
mikakallberg <mikaela.kallberg@hotmail.com>
2022-08-11 14:10:27 +0000

    Add localstorage code

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router";
+import { removeTokenTimestamp, shouldRefreshToken } from "../utils/utils";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -28,20 +29,23 @@ export const CurrentUserProvider = ({ children }) => {
     
     useMemo(() => {
         axiosReq.interceptors.request.use(
-            async (config) => {
-                try {
-                    await axios.post("/dj-rest-auth/token/refresh/");
-                } catch (err) {
-                    setCurrentUser((prevCurrentUser) => {
-                        if (prevCurrentUser) {
-                            history.push("/signin");
+                async (config) => {
+                    if (shouldRefreshToken()) {
+                        try {
+                            await axios.post("/dj-rest-auth/token/refresh/");
+                        } catch (err) {
+                            setCurrentUser((prevCurrentUser) => {
+                                if (prevCurrentUser) {
+                                    history.push("/signin");
+                                }
+                                return null;
+                            });
+                            removeTokenTimestamp();
+                            return config;
                         }
-                        return null;
-                    });
+                    }
                     return config;
-                }
-                return config;
-            },
+                },
             (err) => {
                 return Promise.reject(err);
             }
@@ -60,6 +64,7 @@ export const CurrentUserProvider = ({ children }) => {
                             }
                             return null;
                         });
+                        removeTokenTimestamp();
                     }
                     return axios(err.config);
                 }
